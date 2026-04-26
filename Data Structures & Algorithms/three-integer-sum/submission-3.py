747a4dfa5a24d44b2432f38c65bee45b4a90f724
TeoWeiLer <lerryberry4391@gmail.com>
2026-04-26 22:19:16 +0800

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # hash map 
+        nums.sort() # to organize duplicates and allow easy skipping
+        count = defaultdict(int) # freq map
+        for num in nums:
+            count[num] += 1
+        
+        res = []
+        for i in range(len(nums)): # loop through each index i
+            count[nums[i]] -= 1 # decrease count of nums[i] so it wont be reused
+            if i and nums[i] == nums[i - 1]: # skipping duplicates
+                continue
+
+            for j in range(i + 1, len(nums)): # loop through index j>i
+                count[nums[j]] -= 1 # decrease count
+                if j - 1 > i and nums[j] == nums[j - 1]: # skipping duplicates
+                    continue
+                
+                target = -(nums[i] + nums[j]) # computing k
+                if count[target] > 0:
+                    res.append([nums[i], nums[j], target])
+
+            for j in range(i + 1, len(nums)): 
+                count[nums[j]] += 1
+                # after finishing all j, restore the counts for the second loop 
+                # add back all the decremented values
+                # so that it can be reused for the next i
+
+        return res
